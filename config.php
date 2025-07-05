7baa75367fda60a588b8c7280fcaa3312a5ef4fe
SASCHADDD <saschadanu23@gmail.com>
2025-07-05 11:18:51 +0700

    menambahkan DB socket

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
 // Pengaturan Database
-define('DB_SERVER', '127.0.0.1');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root'); 
 define('DB_PASSWORD', ''); 
 define('DB_NAME', 'pengumpulantugas');
 
+// Menambahkan socket
+define('DB_SOCKET', '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock');
+
 // Membuat koneksi ke database
-$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, null, DB_SOCKET);
 
 // Cek koneksi
 if ($conn->connect_error) {
